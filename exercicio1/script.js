99c6d0ca004bc69cb7372a2852b00e36de7263b9
Luana <luanasouza.dev2022@gmail.com>
2022-07-19 17:44:48 -0300

    exercicio-007-resolvido

---
+++ exercicio1/script.js
@@ -0,0 +1,22 @@
+
+//const nome = ""
+
+let nome = ""
+
+let idade 
+
+console.log (typeof nome)
+console.log (typeof idade)
+//no nome foi impresso string porque é um texto
+//na idade foi impresso indefined porque nãotem valor
+
+nome = prompt("Qual é seu nome?")
+idade = prompt("Qual é sua idade?")
+
+console.log(nome,idade)
+
+console.log(typeof nome)
+console.log(typeof idade)
+//os dois são strings 
+
+console.log("Olá", nome, "você tem", idade, "anos")
